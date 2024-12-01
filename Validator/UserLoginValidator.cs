7f943a535fbf87429e3bc7ae42bc26dfae8c7c9d
phonglam <lamphong1507@gmail.com>
2024-12-01 20:32:22 +0700

    third

--- Validator/UserLoginValidator.cs
+++ Validator/UserLoginValidator.cs
@@ -5,17 +5,19 @@ namespace Company.Validator
 {
     public class UserLoginValidator : AbstractValidator<UserLogin>
     {
-
         public UserLoginValidator()
         {
             RuleFor(user => user.Username)
-    .NotEmpty().WithMessage("Username is required")
-    .Length(3, 10).WithMessage("Username must be between 3 and 10 characters");
+                .NotEmpty()
+                .WithMessage("Username is required")
+                .Length(3, 10)
+                .WithMessage("Username must be between 3 and 10 characters");
 
             RuleFor(user => user.Password)
-                .NotEmpty().WithMessage("Password is required")
-                .MinimumLength(8).WithMessage("Password must be at least 8 characters");
-
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters");
         }
     }
 }
